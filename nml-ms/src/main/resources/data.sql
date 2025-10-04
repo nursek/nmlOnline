85aecc0e9c6f9ba0928f619159ad364b0fb28fff
Lurio <bruno.stier@uha.fr>
2025-10-04 23:18:46 +0200

    Add test users to credentials and fix SQL formatting

--- nml-ms/src/main/resources/data.sql
+++ nml-ms/src/main/resources/data.sql
@@ -7,3 +7,5 @@ CREATE TABLE IF NOT EXISTS CREDENTIALS (
 );
 INSERT INTO CREDENTIALS (username, password, money) VALUES ('test', '$2a$10$WMsQsnTZ/7pFn.klPSeJ0.m0B1bnsAt9wFgkIduzvmkMF2PzvAOUq', 100);
 INSERT INTO CREDENTIALS (username, password, money) VALUES ('a', '$2a$12$ca/.P6xWRGFiH5Ra0UXMk.NhNBxYgCX5aEYDDG3nv9CsaZ1FExMnm', 100);
+INSERT INTO CREDENTIALS (username, password, money) VALUES ('lurio', '$2y$10$PoKeBxBu4AhIM9yMbEUIzOf8SHbdHC8/A5BHqq9jkUT.YiZbsXZNe', 2900);
+INSERT INTO CREDENTIALS (username, password, money) VALUES ('nursek', '$2y$10$X41e/q5zcdbR8T5AMatbFuaXhj.E2fvEJ7DivsuqSlNeY97mrI0mW', 3000);
