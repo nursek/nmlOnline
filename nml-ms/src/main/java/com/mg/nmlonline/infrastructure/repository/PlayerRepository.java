f6148ee7e420f8f7229dfb05e6d6270974f4b5bd
Lurio <bruno.stier@uha.fr>
2025-10-16 00:25:16 +0200

    Refactor PlayerController and PlayerService: change findById to findByName and add findByUsername method in PlayerRepository

--- nml-ms/src/main/java/com/mg/nmlonline/infrastructure/repository/PlayerRepository.java
+++ nml-ms/src/main/java/com/mg/nmlonline/infrastructure/repository/PlayerRepository.java
@@ -2,8 +2,11 @@ package com.mg.nmlonline.infrastructure.repository;
 
 import com.mg.nmlonline.infrastructure.entity.PlayerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
+    @Query("SELECT p FROM PlayerEntity p WHERE p.username = :username")
+    java.util.Optional<PlayerEntity> findByUsername(String username);
 }
